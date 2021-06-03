605bbe5170d7d40582a6f32c4ac3ba142903df93
Danielicy <danielicy@hotmail.com>
2021-06-03 14:02:26 +0300

    time format

--- chess/src/components/Clock/Clock.jsx
+++ chess/src/components/Clock/Clock.jsx
@@ -3,12 +3,32 @@
 //https://dev.to/abdulbasit313/how-to-develop-a-stopwatch-in-react-js-with-custom-hook-561b
 import React from 'react';
 import './clock.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { timers} from '../ClockControls/clockcontrolSlicer'
 
 
 function Clock(props) {
+    
+    const timer = "100";// useSelector(state => state.clock.timers[props.id]);
+
+    function format(time) {
+        const pad = (time, length) => {
+          while (time.length < length) {
+            time = '0' + time;
+          }
+          return time;
+        }
+        
+        time = new Date(time);
+        let m = pad(time.getMinutes().toString(), 2);
+        let s = pad(time.getSeconds().toString(), 2);
+        let ms = pad(time.getMilliseconds().toString(), 3);
+        
+        return `${m} : ${s} . ${ms}`;
+      }
     return (
         <div className="clock">
-            <span>12:30</span>
+             <span>Time:{timer}</span>
         </div>
     );
 }
